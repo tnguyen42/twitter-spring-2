52b82c25f029050e3eb06fec2ae9e0c1e758b8d0
Thanh-Quy Nguyen <thannguy@bouyguestelecom.fr>
2023-06-19 10:07:06 +0200

    :sparkles: Create, Update, Delete tweets

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -13,6 +13,19 @@ contract Twitter {
   Tweet[] public tweets;
 
   /**
+   * A modifier to prevent anyone other than the author of a tweet to execute
+   * the function
+   * @param _id The _id of the concerned tweet
+   */
+  modifier authorOnly(uint256 _id) {
+    require(
+      msg.sender == tweets[_id].author,
+      'Only the author of the tweet can call this function'
+    );
+    _;
+  }
+
+  /**
    * @dev A function that returns all the (non-deleted) tweets.
    * @return Tweet[] An array of all the tweets.
    */
@@ -28,5 +41,48 @@ contract Twitter {
     }
 
     return returnedTweets;
+  }
+
+  /**
+   * @dev A function that creates a new tweet.
+   * @param _content The content of the tweet.
+   */
+  function createTweet(string calldata _content) external {
+    _createTweet(_content);
+  }
+
+  /**
+   * Allows a user to delete one of his tweets
+   * @param _id The id of the tweet to be deleted
+   */
+  function deleteTweet(uint256 _id) external authorOnly(_id) {
+    tweets[_id].deleted = true;
+  }
+
+  /**
+   * Allows a user to update one of his tweets
+   * @param _id The id of the tweet to be updated
+   */
+  function updateTweet(
+    uint256 _id,
+    string calldata _newContent
+  ) external authorOnly(_id) {
+    tweets[_id].content = _newContent;
+  }
+
+  /**
+   * @dev A function that creates a new tweet.
+   * @param _content The content of the tweet.
+   */
+  function _createTweet(string calldata _content) private {
+    tweets.push(
+      Tweet({
+        id: tweets.length,
+        content: _content,
+        timestamp: block.timestamp,
+        author: msg.sender,
+        deleted: false
+      })
+    );
   }
 }
