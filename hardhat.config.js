c50af65a66d50aee401f96fb0ec059641dd09b9e
Thanh-Quy Nguyen <thannguy@bouyguestelecom.fr>
2023-07-03 11:37:55 +0200

    :rocket: Deployment scripts and network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  networks: {
+    goerli_infura: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      timeout: 400000,
+    },
+    goerli_alchemy: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      timeout: 400000,
+    },
+    sepolia_infura: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      timeout: 400000,
+    },
+  },
 };
